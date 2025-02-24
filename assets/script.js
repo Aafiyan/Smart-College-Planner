581c99ed0ea94b0258be24fd5aa5a39317d21e98
Aafiyan <Co.2024.aspathan@bitwardha.ac.in>
2025-02-24 15:49:52 +0530

    Create script.js

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Landing Page Loaded!");
+
+    // Example: Add a click event to the "Register College" button
+    document.querySelector(".register-btn").addEventListener("click", function () {
+        alert("College Registration Page Coming Soon!");
+    });
+});
