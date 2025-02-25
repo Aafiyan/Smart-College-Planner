7e38fefca4c892c86347cb62c2600821aeecd998
Aafiyan <Co.2024.aspathan@bitwardha.ac.in>
2025-02-25 10:44:33 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";  // ✅ Manually added
+import { getFirestore } from "firebase/firestore"; // ✅ Firestore for storing data
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,6 +20,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);  // ✅ Added Firestore (23&24 line added manually)
 const analytics = getAnalytics(app);
 
 // Export Firebase instances
